160209f248ac5d035764b4b3f65c0395b6c4bd66
Emma-Jane Fleetwood <emmafleetwood@gmail.com>
2025-01-11 18:41:29 +0000

    Add toasts

--- bag/views.py
+++ bag/views.py
@@ -41,7 +41,7 @@ def add_to_bag(request, item_id):
     
 
 def adjust_bag(request, item_id):
-    """ Adjust the quantity of the specified product to the shopping bag """
+    """Adjust the quantity of the specified product to the specified amount"""
 
     quantity = int(request.POST.get('quantity'))
     size = None
@@ -60,14 +60,14 @@ def adjust_bag(request, item_id):
         if quantity > 0:
             bag[item_id] = quantity
         else:
-           bag.pop(item_id)
+            bag.pop(item_id)
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
-    
+
 
 def remove_from_bag(request, item_id):
-    """ Remove the specified product from the shopping bag """
+    """Remove the item from the shopping bag"""
 
     try:
         size = None
@@ -79,12 +79,11 @@ def remove_from_bag(request, item_id):
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
-
         else:
             bag.pop(item_id)
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
-    
+
     except Exception as e:
         return HttpResponse(status=500)
